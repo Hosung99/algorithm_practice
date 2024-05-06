852fdca56913cab0415845539a7feba37bd7b33f
Sungho <shson1217@kw.ac.kr>
2024-05-07 01:36:11 +0900

    update: 5/7

---
+++ etc/4562.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int n;
+int main()
+{
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	cin >> n;
+	while (n--)
+	{
+		int x,y;
+		cin >> x >> y;
+		if (x < y)
+			cout << "NO BRAINS\n";
+		else
+			cout << "MMM BRAINS\n";
+	}
+	return (0);
+}
